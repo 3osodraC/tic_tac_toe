dbc55aab1fede7aa52b5ae2ba519c1bf2b4ad994
Vitor Cardoso <vitorcnf64@gmail.com>
2022-07-30 21:02:16 -0300

    added player1, player2 and board instance variables

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,3 +1,12 @@
+# stores all the game's data
 class Game
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
 
+    # 0 = nothing, 1 = player1, 2 = player2
+    @board = [[0, 0, 0][0, 0, 0][0, 0, 0]] # maybe another class for the board?
+  end
+
+  # def play
 end
