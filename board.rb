37a48582658bd0582cfaa0c3057241406f612d39
Vitor Cardoso <vitorcnf64@gmail.com>
2022-08-11 00:01:38 -0300

    added WIN_STATES, might change to a 0-8 instead of 1-9 format later

--- board.rb
+++ board.rb
@@ -1,5 +1,11 @@
 # stores and manipulates the game's board and plays.
 class Board
+  WIN_STATES = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9], # horizontal
+    [1, 4, 7], [2, 5, 8], [3, 6, 9], # vertical
+    [1, 5, 9], [3, 6, 7]             # diagonal
+  ].freeze
+
   def initialize
     @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
