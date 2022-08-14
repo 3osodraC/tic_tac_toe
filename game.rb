96d5ba015ecf0e8b6b02ae6aa8b76198279d9e51
Vitor Cardoso <vitorcnf64@gmail.com>
2022-08-14 20:12:06 -0300

    added prompt_names, removed board.rb. check description.
    i'll try to make everything in a single class, i'll revert if it doesn't work

--- game.rb
+++ game.rb
@@ -1,9 +1,27 @@
 # contains the methods to play the game.
 class Game
+  WIN_STATES = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9], # horizontal
+    [1, 4, 7], [2, 5, 8], [3, 6, 9], # vertical
+    [1, 5, 9], [3, 6, 7]             # diagonal
+  ].freeze
+
   def initialize
     @player1 = nil
     @player2 = nil
+    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
   # def play
+
+  def prompt_names
+    puts "Please enter you names\nPlayer 1: "
+    @player1 = gets.chomp
+    puts 'Player 2: '
+    @player2 = gets.chomp
+  end
+
+  def prompt_plays
+    # code here
+  end
 end
