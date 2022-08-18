1263cc51ed5ae394909273edabbea1069a689bd4
Vitor Cardoso <vitorcnf64@gmail.com>
2022-08-17 21:47:30 -0300

    added prompt_play & validate_play, more in desc.
    may need to move some things i'm planning to add in validate_play to prompt_play

--- game.rb
+++ game.rb
@@ -1,27 +1,40 @@
 # contains the methods to play the game.
 class Game
   WIN_STATES = [
-    [1, 2, 3], [4, 5, 6], [7, 8, 9], # horizontal
-    [1, 4, 7], [2, 5, 8], [3, 6, 9], # vertical
-    [1, 5, 9], [3, 6, 7]             # diagonal
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], # horizontal
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], # vertical
+    [0, 4, 8], [2, 5, 6]             # diagonal
   ].freeze
 
   def initialize
-    @player1 = nil
-    @player2 = nil
-    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @player1 = { name: nil, plays: [] }
+    @player2 = { name: nil, plays: [] }
+    @board = Array.new(9)
   end
 
   # def play
 
   def prompt_names
-    puts "Please enter you names\nPlayer 1: "
-    @player1 = gets.chomp
-    puts 'Player 2: '
-    @player2 = gets.chomp
+    print "Please enter you names\n(X) Player 1: "
+    @player1[:name] = gets.chomp
+    print '(O) Player 2: '
+    @player2[:name] = gets.chomp
   end
 
-  def prompt_plays
+  def prompt_play
+    prompt = ''
+    until prompt.match(/[0-8]/) && prompt.size == 1
+      prompt = gets.chomp
+      prompt.match?(/[0-8]/)
+      p prompt
+    end
+    prompt.to_i
+  end
+
+  def validate_play
     # code here
   end
 end
+
+game = Game.new
+game.prompt_plays
